25f99ca757e1f736b85e973175d6a2dc64c80708
AG4lyf <surajbhari159@gmail.com>
2023-10-07 01:09:35 +0530

    underlying code for yt to spotify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from flask import Response, stream_with_context
 import config
 import json
 from ytmusicapi import YTMusic
@@ -23,50 +24,112 @@ def get_playlist_tracks(playlist_id):
         tracks.extend(results['items'])
     return [track['track'] for track in tracks]
 
-@app.route("/submit")
-def submit():
-    # get the data from the form
-    url = request.args.get('spotify-url', None)
-    if not url:
-        return render_template('home.html')
-    playlist_id = url.split('/')[-1].split('?')[0]
-    #https://open.spotify.com/playlist/54ZA9LXFvvFujmOVWXpHga
-    playlist = get_playlist_tracks(playlist_id)
-    p = spotify.playlist(playlist_id)
-    description = p['description']
-    owner = p['owner']['display_name']
-    name = p['name']
-    tracks = []
-    for track in playlist:
-        track_name = track['name']
-        track_artist = track['artists'][0]['name']
-        year = track['album']['release_date'].split('-')[0]
-        tracks.append({
-            'name': track_name,
-            'artist': track_artist,
-            'year': year
-        })
-    yt_playlist = yt.create_playlist(name, description=description)
-    link = f"https://music.youtube.com/playlist?list={yt_playlist}"
-    print(link)
-    count = 0
-    total = len(tracks)
-    for track in tracks:
-        search = yt.search(f'{track["name"]} {track["artist"]} {track["year"]}')
-        if not search:
-            continue
+@app.route("/styt")
+def styt():
+    def gen():
         try:
-            yt.add_playlist_items(yt_playlist, [search[0]['videoId']])
-        except KeyError:
-            continue
-        count += 1
-        print(f"{count}/{total} songs added")
-    description = f'Created by {owner} on Spotify. Link to original playlist: {url} . Added {count} out of {total} songs. ... {description}'
-    yt.edit_playlist(
-        yt_playlist, 
-        privacyStatus='PUBLIC', 
-        description=description)
-    return redirect(link, code=302)
+            # get the data from the form
+            url = request.args.get('spotify-url', None)
+            if not url:
+                return render_template('home.html')
+            playlist_id = url.split('/')[-1].split('?')[0]
+            #https://open.spotify.com/playlist/54ZA9LXFvvFujmOVWXpHga
+            playlist = get_playlist_tracks(playlist_id)
+            p = spotify.playlist(playlist_id)
+            description = p['description']
+            owner = p['owner']['display_name']
+            name = p['name']
+            tracks = []
+            for track in playlist:
+                track_name = track['name']
+                track_artist = track['artists'][0]['name']
+                year = track['album']['release_date'].split('-')[0]
+                tracks.append({
+                    'name': track_name,
+                    'artist': track_artist,
+                    'year': year
+                })
+            yt_playlist = yt.create_playlist(name, description=description)
+            link = f"https://music.youtube.com/playlist?list={yt_playlist}"
+            print(link)
+            count = 0
+            total = len(tracks)
+            for track in tracks:
+                search = yt.search(f'{track["name"]} {track["artist"]} {track["year"]}')
+                if not search:
+                    continue
+                try:
+                    yt.add_playlist_items(yt_playlist, [search[0]['videoId']])
+                except KeyError:
+                    continue
+                count += 1
+                print(f"{count}/{total} songs added")
+                yield f"{count}/{total} songs added </br>"
+            description = f'Created by {owner} on Spotify. Link to original playlist: {url} . Added {count} out of {total} songs. ... {description}'
+            yt.edit_playlist(
+                yt_playlist, 
+                privacyStatus='PUBLIC', 
+                description=description)
+            print('done')
+            # clear the html page
+            yield "done"
+            yield "<script>document.body.innerHTML = 'REDIRECTING';</script>"
+            yield f"<script>window.location = '{link}';</script>"
+        except GeneratorExit:
+            print('closed')
+            yt.delete_playlist(yt_playlist)
 
+    return Response(stream_with_context(gen()))
+
+
+@app.route("/ytts")
+def ytts():
+    def gen():
+        try:
+            # get the data from the form
+            url = request.args.get('yt-url', None)
+            if not url:
+                return render_template('home.html')
+            playlist_id = url.split("?list=")[-1]
+            playlist = yt.get_playlist(playlist_id)
+            with open('playlist.json', 'w') as f:
+                json.dump(playlist, f, indent=4)
+            tracks = []
+            name = playlist['title']
+            description = playlist['description'] if playlist['description'] else ''
+            for track in playlist["tracks"]:
+                track_name = track['title']
+                track_artist = track['artists'][0]['name']
+                tracks.append({
+                    'name': track_name,
+                    'artist': track_artist,
+                })
+            playlist = spotify.user_playlist_create(
+                user=user_id,
+                name=playlist['title'], 
+                description=description,
+                public=True)
+            count = 0
+            total = len(tracks)
+            for track in tracks:
+                search = spotify.search(f'{track["name"]} {track["artist"]}', limit=1)
+                if not search:
+                    continue
+                try:
+                    spotify.playlist_add_items(playlist['id'], [search['tracks']['items'][0]['uri']])
+                except KeyError:
+                    continue
+                count += 1
+                print(f"{count}/{total} songs added")
+                yield f"{count}/{total} songs added </br>"
+            print('done')
+            yield "<script>document.body.innerHTML = 'REDIRECTING';</script>"
+            yield f"<script>window.location = 'https://open.spotify.com/playlist/{playlist}';</script>"
+        except GeneratorExit:
+            print('closed')
+            spotify.user_playlist_unfollow(config.user_id, playlist['id'])
+
+    return Response(stream_with_context(gen()))
+        
 if __name__ == '__main__':
     app.run(debug=True)
